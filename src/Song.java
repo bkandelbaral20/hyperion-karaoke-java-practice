9265803bf086f1c180b81697d0cf819d2862a0fd
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-08-24 13:48:47 -0500

    exercises

---
+++ src/Song.java
@@ -0,0 +1,44 @@
+public class Song {
+    //    private string properties for title, artist, and a private list of strings for lyrics
+    private String title;
+    private String artist;
+    private String lyrics;
+
+    //    a constructor to assign values to all instance properties
+    public Song(String title, String artist, String lyrics) {
+        this.title = title;
+        this.artist = artist;
+        this.lyrics = lyrics;
+    }
+
+    //     getters and setters for all instance properties
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getLyrics() {
+        return lyrics;
+    }
+
+    public void setLyrics(String lyrics) {
+        this.lyrics = lyrics;
+    }
+
+    //    a static method parseLyrics that takes in a single string input of possibly several words and returns a
+//    list of strings that are the output of splitting all the words in the input string on a single space character " "
+    public static String parseLyrics(String input) {
+        return input;
+    }
+}
