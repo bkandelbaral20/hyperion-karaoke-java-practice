4d13b297dd3395b740f92f1fe58a99d6058d2ba4
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-08-24 13:55:03 -0500

    exercises

---
+++ src/Album.java
@@ -0,0 +1,37 @@
+public class Album {
+//    a private string property for the album name
+//    a private property for a list of song objects called songs
+    private String name, songs;
+
+//    a constructor that sets both instance properties
+
+    public Album(String name, String songs) {
+        this.name = name;
+        this.songs = songs;
+    }
+
+//    getters and setters for both instance properties
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSongs() {
+        return songs;
+    }
+
+    public void setSongs(String songs) {
+        this.songs = songs;
+    }
+
+//    a public method with a void return type called printTrackListings that will iterate through
+//    all songs for a given album
+
+    public void printTrackListings(){
+        for
+    }
+}
