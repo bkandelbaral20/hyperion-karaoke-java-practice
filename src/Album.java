1b7f38acc005eb9f511a11ee0534dc11135a0c90
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-08-26 09:13:39 -0500

    exercises

--- src/Album.java
+++ src/Album.java
@@ -1,13 +1,17 @@
+import java.util.ArrayList;
+
 public class Album {
 
     //    a private string property for the album name
 //    a private property for a list of song objects called songs
     private String name;
-    private String songs;
+    private String[] songs;
+
+    public Album(){};
 
 //    a constructor that sets both instance properties
 
-    public Album(String name, String songs) {
+    public Album(String name, String[] songs) {
         this.name = name;
         this.songs = songs;
     }
@@ -22,24 +26,30 @@ public class Album {
         this.name = name;
     }
 
-    public String getSongs() {
+    public String[] getSongs() {
         return songs;
     }
 
-    public void setSongs(String songs) {
+    public void setSongs(String[] songs) {
         this.songs = songs;
     }
 
 //    a public method with a void return type called printTrackListings that will iterate through
 //    all songs for a given album
 
-//    public static void main(String[] args) {
-//        printTrackListings();
-//    }
-    public void printTrackListings(){
-        System.out.println("90S HITS track listing.....");
-        for(int i =0; i < songs.length(); i++){
-            System.out.println(songs);
+    public static void main(String[] args) {
+        String[] songs = {"Infinity", "Infinity 2", "Infinity 3"};
+        Album album = new Album("XX", songs);
+
+        //calling method to run
+        album.printTrackListings(album);
+    }
+
+    public void printTrackListings(Album album) {
+        System.out.println(name);
+        String[] trackList = album.getSongs();
+        for (String str : trackList) {
+            System.out.println( str);
         }
     }
 }
--- src/Playable.java
+++ src/Playable.java
@@ -2,5 +2,5 @@
 public interface Playable {
 //    a method called play that takes in an album instance with a void return type
 //    a method called play that takes in a song instance with a void return type
-    public void play();
+
 }
